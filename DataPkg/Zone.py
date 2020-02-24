43f3e94d7525ab30827c5cbc7c7429947a4f3566
LotteHendrickx <lotte.hendrickx@outlook.com>
2020-02-24 10:15:46 +0100

    Made Zone.py

---
+++ DataPkg/Zone.py
@@ -0,0 +1,13 @@
+class Zone:
+    def __init__(self,name):
+        self.name=name
+        self.zones=[]
+
+    def getName(self):
+        return self.name
+
+    def getZones(self)
+        return self.zones
+
+    def addZone(self,z)
+        self.zones.add(z)
