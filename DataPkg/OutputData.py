340d960e3b4ab5612844fa6f7b193c5b33ff0c39
laurens-wuyts <laurens.wuyts@gmail.com>
2020-04-23 20:28:12 +0200

    Commentaar toevoegen en code opkuisen

--- DataPkg/OutputData.py
+++ DataPkg/OutputData.py
@@ -5,65 +5,96 @@ from DataPkg.Zone import Zone
 import random
 import csv
 
-import inspect
-
 
 class OutputData:
+	"""Holds the contents for the search algorithm and has methods to do specific types of searches."""
+
 	def __init__(self):
-		self.cost 	   = 0
-		self.carZones  = dict()	# {carID: zone, carID: zone}
-		self.resCars   = dict() # {res: car, res: car}
-		self.unassigned= []     # [res, res]
-		self.usedCars  = dict() # {carID: [(t1, d1), (t2, d2)], carID: [(t1, d1), (t2, d2)]}
-		random.seed()
+		"""
+		Initialise all internal lists and dictionaries to have empty elements.
+
+		Structures of all dictionaries and lists:
+			carZones: 	{carID: zone, carID: zone}
+			resCars:	{res: car, res: car}
+			unassigned:	[res, res]
+			usedCars:	{carID: [(t1, d1), (t2, d2)], carID: [(t1, d1), (t2, d2)]}
+		"""
+		self.cost 	   = 0		# The cost of this solution
+		self.carZones  = dict()	# Define for every car a zone
+		self.resCars   = dict() # Connect all reservations to a specific car
+		self.unassigned= []     # All reservations that have no solution
+		self.usedCars  = dict() # Keep a timetable for all cars 
 
 	def localSearch(self):
-		l= list(self.resCars.items())
-		random.shuffle(l)
-		self.resCars   = dict(l)
-		searchOps 	   = [self.changeManyCars,self.changeCar,self.changeReservation]
-		searchFunction = random.choice(searchOps)
-		searchFunction()
+		"""
+		Execute local search on the data.
+
+		Shuffles all reservations linked to cars and than
+		picks a random function from the list to apply to the data.
+		"""
+
+		l = list(self.resCars.items())	# Convert the dictionary to a list
+		random.shuffle(l)				# Shuffle the list at random
+		self.resCars   = dict(l)		# Convert the list back to a dictionary
+		searchOps 	   = [				# A list of the different functions that can be picked
+			self.changeManyCars,
+			self.changeCar,
+			self.changeReservation
+		]
+		searchFunction = random.choice(searchOps)	# Pick a function from the list at random
+		searchFunction()				# Execute the function on the data
 
 	def changeManyCars(self):
-		amountCars     = int(len(self.carZones) * 0.7)
-		amountShuffles = int(amountCars // 2)
-		delete = []
-		zones  = []
-		cars   = random.sample(list(self.carZones),amountCars)
-		for c in cars:
+		"""
+		Pick a number of cars from the list and shuffle their zones around
+		
+		This is quite a big jump for local search, but it prevents local minimum.
+		The function takes 70% of all cars from the list and shuffles their zones 
+		half the number of cars were picked.
+		"""
+		amountCars     = int(len(self.carZones) * 0.7)	# Get the amount of 70% of cars
+		amountShuffles = int(amountCars // 2)			# Divide it by 2 for the amount of shuffles
+		delete = []		# A list to keep track of all the reservations that need to be removed from the resCars list
+		zones  = []		# A list to keep all the zones from the cars in while shuffling
+		cars   = random.sample(list(self.carZones),amountCars)	# Sample 70% of the cars from the list
+		for c in cars:	# Get all zones from the cars and clear the usage table
 			zones.append(self.carZones[c])
 			self.usedCars[c] = []
-		for res,car in self.resCars.items():
+		for res,car in self.resCars.items():	# Find all reservations connected to those cars to be removed
 			if car in cars:
 				delete.append(res)
 				self.unassigned.append(res)
-		for d in delete:
+		for d in delete:						# Remove the reservations from the list
 			del self.resCars[d]
 
-		for _ in range(amountShuffles):
+		for _ in range(amountShuffles):			# Shuffle all zones
 			random.shuffle(zones)
-		for idx in range(amountCars):
+		for idx in range(amountCars):			# Reconnect the cars and the shuffled zones
 			self.carZones[cars[idx]] = zones[idx]
-		self.reassign()
+		self.reassign()	# Try to reassign as much of the unassigned reservations as possible
 
 	def changeCar(self):
-		car,zone = random.choice(list(self.carZones.items()))
-		new_zone = random.choice(zone.getZonesObj())
-		delete   = []
-		self.carZones[car] = new_zone
-		for r,c in self.resCars.items():
+		"""
+		Switch a car to a neighbouring zone
+
+		Pick a random car and switch it to a random connected neighbouring zone
+		"""
+		car,zone = random.choice(list(self.carZones.items()))	# Pick a random car from the list
+		new_zone = random.choice(zone.getZonesObj())			# Pick a random neighbour zone from the previous zone of the car
+		delete   = []	# A list to keep track of all the reservations that need to be removed from the resCars list 
+		self.carZones[car] = new_zone		# Apply the new zone to the selected car
+		for r,c in self.resCars.items():	# For all reservations that used this car, ...
 			if c is car:
-				if not self.checkZone(r.getZoneObj(),c):
-					delete.append(r)
-					for i in range(len(self.usedCars[c])):
+				if not self.checkZone(r.getZoneObj(),c):	# Check if the zone is not to far for the reservation
+					delete.append(r)						# If not, remove the reservation
+					for i in range(len(self.usedCars[c])):	# Find the used timeslot of the reservation and clear it
 						if int(self.usedCars[c][i][0]) == int(r.getStart()):
 							self.usedCars[c].pop(i)
 							break
-		for res in delete:
+		for res in delete:			# Remove the reservations from the list
 			del self.resCars[res]
 			self.unassigned.append(res)
-		self.reassign()
+		self.reassign()	# Try to reassign as much of the unassigned reservations as possible
 
 	def changeReservation(self):
 		res,car   = None,None
@@ -105,16 +136,7 @@ class OutputData:
 		deletepop = []
 		timeslot  = self.checkTime(chosenCar,res.getStart(),res.getDuration())
 		if timeslot > 0:
-			s = res.getStart()
-			d = res.getDuration()
-			if timeslot   == 1 :
-				self.usedCars[chosenCar].append((s,d))
-			elif timeslot == 2 :
-				self.usedCars[chosenCar].insert(0, (s, d))
-			elif timeslot == 3 :
-				self.usedCars[chosenCar].append((s, d))
-			else:
-				self.usedCars[chosenCar].insert(timeslot - 3, (s, d))
+			self.addTimeslot(timeslot, res, chosenCar)
 		else:
 			s2 = int(res.getStart())
 			d2 = int(res.getDuration())
@@ -134,16 +156,7 @@ class OutputData:
 
 			timeslot = self.checkTime(chosenCar,res.getStart(),res.getDuration())
 			if timeslot > 0:
-				s = res.getStart()
-				d = res.getDuration()
-				if timeslot   == 1 :
-					self.usedCars[chosenCar].append((s,d))
-				elif timeslot == 2 :
-					self.usedCars[chosenCar].insert(0, (s, d))
-				elif timeslot == 3 :
-					self.usedCars[chosenCar].append((s, d))
-				else:
-					self.usedCars[chosenCar].insert(timeslot - 3, (s, d))
+				self.addTimeslot(timeslot, res, chosenCar)
 		self.reassign()
 
 	def reassign(self):
@@ -166,16 +179,7 @@ class OutputData:
 			if match:
 				delete.append(idx)
 				i = self.resCars[r]
-				s = r.getStart()
-				d = r.getDuration()
-				if timeslot   == 1 :
-					self.usedCars[i].append((s,d))
-				elif timeslot == 2 :
-					self.usedCars[i].insert(0, (s, d))
-				elif timeslot == 3 :
-					self.usedCars[i].append((s, d))
-				else:
-					self.usedCars[i].insert(timeslot-3, (s, d))
+				self.addTimeslot(timeslot, r, i)
 
 		for c,i in enumerate(delete):
 			self.unassigned.pop(i-c)
@@ -198,6 +202,18 @@ class OutputData:
 				dy = int(dy)
 				if (s >= (sx + dx) and (s + d) <= sy):	return 4 + idx
 			return 0
+
+	def addTimeslot(self, ts, res, car):
+		s = res.getStart()
+		d = res.getDuration()
+		if ts   == 1 :
+			self.usedCars[car].append((s,d))
+		elif ts == 2 :
+			self.usedCars[car].insert(0, (s, d))
+		elif ts == 3 :
+			self.usedCars[car].append((s, d))
+		else:
+			self.usedCars[car].insert(ts - 3, (s, d))
 
 	def checkZone(self, zone, i):
 		z = self.carZones[i]
@@ -250,16 +266,7 @@ class OutputData:
 				self.unassigned.append(r)
 			else:
 				i = self.resCars[r]
-				s = r.getStart()
-				d = r.getDuration()
-				if timeslot   == 1 :
-					self.usedCars[i].append((s,d))
-				elif timeslot == 2 :
-					self.usedCars[i].insert(0, (s, d))
-				elif timeslot == 3 :
-					self.usedCars[i].append((s, d))
-				else:
-					self.usedCars[i].insert(timeslot - 3, (s, d))
+				self.addTimeslot(timeslot, r, i)
 
 		for c,z in self.carZones.items():
 			if z is None:
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import sys
 import copy
+import time
 import random
 
 from multiprocessing import Pool
@@ -11,9 +12,9 @@ from DataPkg.OutputData import OutputData as OutD
 
 in_file   = sys.argv[1]
 out_file  = sys.argv[2]
-time_lim  = sys.argv[3]
-rand_seed = sys.argv[4]
-threads   = sys.argv[5]
+time_lim  = int(sys.argv[3])
+rand_seed = int(sys.argv[4])
+threads   = int(sys.argv[5])
 
 random.seed(rand_seed)
 
@@ -24,12 +25,17 @@ outd = OutD()
 outd.initialise(ind)
 outd2=copy.deepcopy(outd)
 
-for i in range(10000):
+timeout_start = time.time()
+idx = 0
+
+while time.time() < timeout_start + time_lim:
+    idx += 1
     outd2.localSearch()
-    if outd.getCost()>outd2.getCost():
-        outd=copy.deepcopy(outd2)
-        print('Best',outd2.getCost())
+    if outd.getCost() > outd2.getCost():
+        outd = copy.deepcopy(outd2)
+        print('Best', outd2.getCost())
     else:
-        outd2=copy.deepcopy(outd)
+        outd2 = copy.deepcopy(outd)
 
 outd.saveCSV(out_file)
+print("Iter:", idx)
